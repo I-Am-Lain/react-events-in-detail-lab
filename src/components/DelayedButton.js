a46970ef0d6ea6bf457550da5734b5d2343b8536
I-Am-Lain <vetter.william@gmail.com>
2020-11-17 22:21:35 -0600

    Fin. Events don't pool anymore

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,20 @@
 // Code DelayedButton Component Here
+
+import React,{Component} from 'react'
+
+
+export default class DelayedButton extends Component{
+    
+    handleClick = (e) => {
+        setTimeout(() => this.props.onDelayedClick(e), this.props.delay)
+    }
+    
+    render(){
+        return(
+            <button type='button' 
+            onClick={() => this.handleClick(e.persist())}>
+                Delay
+            </button>
+        )
+    }
+}
